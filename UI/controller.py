691b3c95ed98e73475a0c336ad75895240ba8421
Giuseppe Averta <g.averta3@gmail.com>
2024-05-10 14:57:57 +0200

    startup gui

--- UI/controller.py
+++ UI/controller.py
@@ -8,10 +8,9 @@ class Controller:
         # the model, which implements the logic of the program and holds the data
         self._model = model
 
-    def handle_hello(self, e):
-        name = self._view.txt_name.value
-        if name is None or name == "":
-            self._view.create_alert("Inserire il nome")
-            return
-        self._view.txt_result.controls.append(ft.Text(f"Hello, {name}!"))
-        self._view.update_page()
+    def handleAnalizzaOggetti(self, e):
+        pass
+
+    def handleCompConnessa(self,e):
+        pass
+
--- UI/view.py
+++ UI/view.py
@@ -6,9 +6,9 @@ class View(ft.UserControl):
         super().__init__()
         # page stuff
         self._page = page
-        self._page.title = "Template application using MVC and DAO"
+        self._page.title = "TdP Exercise on MIA Art database"
         self._page.horizontal_alignment = 'CENTER'
-        self._page.theme_mode = ft.ThemeMode.DARK
+        self._page.theme_mode = ft.ThemeMode.LIGHT
         # controller (it is not initialized. Must be initialized in the main, after the controller is created)
         self._controller = None
         # graphical elements
@@ -20,22 +20,23 @@ class View(ft.UserControl):
 
     def load_interface(self):
         # title
-        self._title = ft.Text("Hello World", color="blue", size=24)
+        self._title = ft.Text("The MIA Collection database", color="orange", size=24)
         self._page.controls.append(self._title)
 
-        #ROW with some controls
-        # text field for the name
-        self.txt_name = ft.TextField(
-            label="name",
-            width=200,
-            hint_text="Insert a your name"
-        )
+        # controls
+        self._btnAnalizzaOggetti = ft.ElevatedButton(text="Analizza oggetti",
+                                                     on_click=self._controller.handleAnalizzaOggetti,
+                                                     bgcolor="orange",
+                                                     color="white",
+                                                     width=200)
+        self._txtIdOggetto = ft.TextField(label="Id Oggetto", color="orange", border_color="orange")
+        self._btnCompConnessa = ft.ElevatedButton(text="Cerca Connessa", on_click=self._controller.handleCompConnessa,
+                                                  bgcolor="orange",
+                                                  color="white",
+                                                  width=200)
 
-        # button for the "hello" reply
-        self.btn_hello = ft.ElevatedButton(text="Hello", on_click=self._controller.handle_hello)
-        row1 = ft.Row([self.txt_name, self.btn_hello],
-                      alignment=ft.MainAxisAlignment.CENTER)
-        self._page.controls.append(row1)
+        self._page.controls.append(ft.Row([self._btnAnalizzaOggetti, self._txtIdOggetto, self._btnCompConnessa],
+                                          alignment=ft.MainAxisAlignment.CENTER))
 
         # List View where the reply is printed
         self.txt_result = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
@@ -45,19 +46,11 @@ class View(ft.UserControl):
     @property
     def controller(self):
         return self._controller
-
     @controller.setter
     def controller(self, controller):
         self._controller = controller
 
     def set_controller(self, controller):
         self._controller = controller
-
-    def create_alert(self, message):
-        dlg = ft.AlertDialog(title=ft.Text(message))
-        self._page.dialog = dlg
-        dlg.open = True
-        self._page.update()
-
     def update_page(self):
         self._page.update()
